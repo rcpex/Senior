d9640c6030327ce529b8e72b04807149940661a5
rcpex <rcpsp1@hotmail.com>
2017-12-08 00:50:44 -0200

    cidadeController, cidadeRepository, correcao itens 9 e 10 do pdf

--- src/main/java/Senior/controllers/CidadeController.java
+++ src/main/java/Senior/controllers/CidadeController.java
@@ -3,12 +3,14 @@ package Senior.controllers;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import Senior.models.Cidade;
 import Senior.repository.CidadeRepository;
@@ -24,6 +26,7 @@ public class CidadeController {
 	}
 	
 	@RequestMapping("/carrega-db")
+	@ResponseBody
 	public void carregarCidades() {		
 		String arquivo =  "C:/Users/Renan/Desktop/Trabalho Java - Cidades.csv";
 	    BufferedReader br = null;
@@ -71,11 +74,13 @@ public class CidadeController {
 	}
 	
 	@RequestMapping("/capitais")
+	@ResponseBody
 	public List<Cidade> getCapitais() {	
 		return repository.getCapitaisOrdenadasPorNome();
 	}
 	
 	@RequestMapping("/maior-menor-qtde")
+	@ResponseBody
 	public Map<String, Long> getUfMaiorMenorQtdeCidade() {
 	
 		List<Object[]> qtdeCidadePorEstado = repository.getQtdeCidadesPorEstado();
@@ -113,28 +118,32 @@ public class CidadeController {
 	}
 	
 	@RequestMapping("/qtde-cidades")
+	@ResponseBody
 	public Map<String, Long> getQtdeCidadePorEstado() {
 		
-		Map<String, Long> ufQtdeCidades  = new HashMap<String, Long>();
+		Map<String, Long> result  = new HashMap<String, Long>();
 		
 		for(Object[] aux : repository.getQtdeCidadesPorEstado()) {
-			ufQtdeCidades.put((String) aux[0], (Long) aux[1]);
+			result.put((String) aux[0], (Long) aux[1]);
 		}
 
-		return ufQtdeCidades;
+		return result;
 	}
 	
 	@RequestMapping("/cidade-por-ibge")
+	@ResponseBody
 	public Cidade getCidadePorIbge(String ibgeId) {		
 		return repository.findByIbgeId(ibgeId);
 	}
 	
 	@RequestMapping("/cidades-por-uf")
+	@ResponseBody
 	public List<Cidade> getCidadesPorEstado(String uf) {		
 		return repository.findByUf(uf);
 	}
 	
 	@RequestMapping("/salvar-cidade")
+	@ResponseBody
 	public Cidade salvarCidade(Cidade cidade) {		
 		return repository.save(cidade);
 	}
@@ -145,43 +154,103 @@ public class CidadeController {
 	}
 	
 	@RequestMapping("/cidades-por-filtro")
-	public List<Cidade> getRegistrosPorColuna(String coluna, String filtro) {	
-		return repository.getCidadesPorFiltro(coluna, filtro);
-	}
+	@ResponseBody
+	public List<Cidade> getRegistrosPorColuna(String coluna, String filtro) {
 	
+		if(coluna.equals("ibgeId")) {
+			return repository.getCidadesPorIbgeId(filtro);
+		} else if(coluna.equals("uf")) {
+			return repository.getCidadesPorEstado(filtro);
+		} else if(coluna.equals("name")) {
+			return repository.getCidadesPorName(filtro);
+		} else if(coluna.equals("alternativeNames")) {
+			return repository.getCidadesPorAlternativeNames(filtro);
+		} else if(coluna.equals("noAccents")) {
+			return repository.getCidadesPorNoAccents(filtro);
+		} else if(coluna.equals("microregion")) {
+			return repository.getCidadesPorMicroregion(filtro);
+		} else if(coluna.equals("mesoregion")) {
+			return repository.getCidadesPorMesoregion(filtro);
+		}		
+			
+		return null;
+	}
+		
 	@RequestMapping("/qtde-registros-coluna")
-	public String getRegistrosPorColuna(String coluna) {		
-		return String.valueOf(repository.getQtdeRegistrosPorColuna(coluna));
+	@ResponseBody
+	public Map<String, Long> getQtdeRegistrosPorColuna(String coluna) {	
+		
+		Map<String, Long> result  = new HashMap<String, Long>();
+		
+		if(coluna.equals("uf")) {
+			for(Object[] aux : repository.getQtdeCidadesPorEstado()) {
+				result.put((String) aux[0], (Long) aux[1]);
+			}
+		} else if(coluna.equals("microregion")) {
+			for(Object[] aux : repository.getQtdeCidadesPorMicroregion()) {
+				result.put((String) aux[0], (Long) aux[1]);
+			}
+		} else if(coluna.equals("mesoregion")) {
+			for(Object[] aux : repository.getQtdeCidadesPorMesoregion()) {
+				result.put((String) aux[0], (Long) aux[1]);
+			}
+		} else if(coluna.equals("name")) {
+			for(Object[] aux : repository.getQtdeCidadesPorName()) {
+				result.put((String) aux[0], (Long) aux[1]);
+			}
+		} else {
+			result.put("Modifique sua busca para as colunas name, uf, microregion ou mesoregion", new Long(0));
+		}
+		
+		return result;
 	}
 	
 	@RequestMapping("/qtde-registros")
-	public String getQtdeRegistros() {		
-		System.out.println(String.valueOf(repository.getQtdeRegistros()));
-		return String.valueOf(repository.getQtdeRegistros());
+	@ResponseBody
+	public Map<String, String> getQtdeRegistros() {		
+		Map<String, String> result = new HashMap<>();
+		
+		result.put("qtde-registros", String.valueOf(repository.getQtdeRegistros()));
+		return result;
 	}
 	
 	@RequestMapping("/maior-distancia")
-	public String getMaiorDistancia() {		
+	@ResponseBody
+	public Map<String, String> getMaiorDistancia() {		
 		double cat1, cat2, hip, dist=0;
 		Cidade cidade1 = null, cidade2 = null;
 		
-		List<Cidade> cidades1 = repository.findAll();
-		List<Cidade> cidades2 = repository.findAll();
+		List<Cidade> cidades = new ArrayList<>();
+		cidades = repository.findAll();
 		
-		for(Cidade c1 : cidades1) {		
-			for(Cidade c2 : cidades2) {
-				cat1 = Math.pow(c1.getLon() - c2.getLon(), 2);
-				cat2 = Math.pow(c1.getLat() - c2.getLat(), 2);
-				hip = Math.sqrt(cat1+cat2);
-				if(dist < hip) {
-					dist = hip;
-					cidade1 = c1;
-					cidade2 = c2;
-				}
-			}			
-		}
+		int i = 0, j = 1;
+		while(i < cidades.size()) {
+			
+			cat1 = Math.pow(cidades.get(j).getLon() - cidades.get(i).getLon(), 2);
+			cat2 = Math.pow(cidades.get(j).getLat() - cidades.get(i).getLat(), 2);
+			hip = Math.sqrt(cat1+cat2);
+
+			if(dist < hip) {
+				dist = hip;
+				cidade1 = cidades.get(i);
+				cidade2 = cidades.get(j);
+			}
+			
+			i++;
+			
+			if (i == cidades.size()) {
+				j++;
+				i = j+1; 
+			}
+		}			
 		
-		String maiorDistancia = cidade1.getName() + " - " + cidade2.getName();
-		return maiorDistancia;
+		String maiorDistancia = cidade1.getName() + ", " + cidade1.getUf() 
+								+ " - " 
+								+ cidade2.getName()+ ", " + cidade2.getUf();
+
+		Map<String, String> result = new HashMap<>();
+		result.put("Maior distancia", maiorDistancia);
+		
+		return result;
 	}
 }
--- src/main/java/Senior/repository/CidadeRepository.java
+++ src/main/java/Senior/repository/CidadeRepository.java
@@ -22,15 +22,39 @@ public interface CidadeRepository extends CrudRepository<Cidade, String> {
 	
 	@Query("SELECT c FROM Cidade c WHERE c.capital is true ORDER BY c.name")
 	List<Cidade> getCapitaisOrdenadasPorNome();
+
+	@Query("SELECT c FROM Cidade c WHERE c.ibgeId LIKE %:filtro%")
+	List<Cidade> getCidadesPorIbgeId(@Param("filtro") String filtro);
+	
+	@Query("SELECT c FROM Cidade c WHERE c.uf LIKE %:filtro%")
+	List<Cidade> getCidadesPorEstado(@Param("filtro") String filtro);
+	
+	@Query("SELECT c FROM Cidade c WHERE c.name LIKE %:filtro%")
+	List<Cidade> getCidadesPorName(@Param("filtro") String filtro);
+	
+	@Query("SELECT c FROM Cidade c WHERE c.noAccents LIKE %:filtro%")
+	List<Cidade> getCidadesPorNoAccents(@Param("filtro") String filtro);
+	
+	@Query("SELECT c FROM Cidade c WHERE c.alternativeNames LIKE %:filtro%")
+	List<Cidade> getCidadesPorAlternativeNames(@Param("filtro") String filtro);
+	
+	@Query("SELECT c FROM Cidade c WHERE c.microregion LIKE %:filtro%")
+	List<Cidade> getCidadesPorMicroregion(@Param("filtro") String filtro);
+	
+	@Query("SELECT c FROM Cidade c WHERE c.mesoregion LIKE %:filtro%")
+	List<Cidade> getCidadesPorMesoregion(@Param("filtro") String filtro);
 	
 	@Query("SELECT c.uf, COUNT(c) FROM Cidade c GROUP BY c.uf")
 	List<Object[]> getQtdeCidadesPorEstado();
 	
-	@Query("SELECT c FROM Cidade c WHERE :coluna = :filtro")
-	List<Cidade> getCidadesPorFiltro(@Param("coluna") String coluna, @Param("filtro") String filtro);
+	@Query("SELECT c.microregion, COUNT(c) FROM Cidade c GROUP BY c.microregion")
+	List<Object[]> getQtdeCidadesPorMicroregion();
 	
-	@Query("SELECT COUNT(c) FROM Cidade c group by :coluna")
-	Integer getQtdeRegistrosPorColuna(@Param("coluna") String coluna);
+	@Query("SELECT c.mesoregion, COUNT(c) FROM Cidade c GROUP BY c.mesoregion")
+	List<Object[]> getQtdeCidadesPorMesoregion();
+	
+	@Query("SELECT c.name, COUNT(c) FROM Cidade c GROUP BY c.name")
+	List<Object[]> getQtdeCidadesPorName();
 	
 	@Query("SELECT COUNT(c) FROM Cidade c")
 	int getQtdeRegistros();
